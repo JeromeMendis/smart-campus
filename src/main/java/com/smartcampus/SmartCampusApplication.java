6894b0f578ad0b013403931fe799afec4c4af77a
JeromeMendis <w2120287@westminster.ac.uk>
2026-04-23 22:31:44 +0530

    adding smart campus application

---
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -0,0 +1,11 @@
+package com.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*
+JAX-RS Application configuration class.
+*/
+@ApplicationPath("/")
+public class SmartCampusApplication extends Application {
+}
